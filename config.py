fdfbdb5e991599ec77852d1b810e0e87dc1b4b48
OGxFUBAR <Rcmaple92@gmail.com>
2025-01-28 11:08:43 -0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
