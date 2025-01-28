b6485389473a00258a804aeccbde57996b7963fb
OGxFUBAR <Rcmaple92@gmail.com>
2025-01-28 12:07:41 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -37,3 +37,13 @@ class Archive(db.Model):
     vehicle_number = db.Column(db.String(50), nullable=False)
     last_cleaned_at = db.Column(db.DateTime, nullable=True)
     last_reserved_at = db.Column(db.DateTime, nullable=False)
+
+from flask_login import UserMixin
+from . import db
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(20), default='user')  # 'admin' for admins
+
